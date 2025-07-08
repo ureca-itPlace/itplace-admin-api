f019e040c2edab8f7ec17c553467dfe3e76a982b
Eddie Lee <devleehy@gmail.com>
2025-07-08 16:39:26 +0900

    init project

---
+++ src/main/java/com/itplace/adminapi/ItplaceAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.itplace.adminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItplaceAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItplaceAdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itplace/adminapi/ItplaceAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itplace.adminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItplaceAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
