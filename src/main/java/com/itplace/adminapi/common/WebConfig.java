4fd9b3c23be8d1c0aa51457a5e9d9a2d198150e9
rxgx424 <gkfudrd1234@gmail.com>
2025-07-17 13:47:37 +0900

    add common

---
+++ src/main/java/com/itplace/adminapi/common/WebConfig.java
@@ -0,0 +1,17 @@
+package com.itplace.adminapi.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 대해
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:5174") // 허용할 Origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키 허용 여부
+    }
+}
