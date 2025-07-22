1329a793d495616ddcafc1ccbf796252199bad6a
Eddie Lee <devleehy@gmail.com>
2025-07-22 15:50:09 +0900

    feat: Role 로직 추가

--- src/main/java/com/itplace/adminapi/user/entity/Role.java
+++ src/main/java/com/itplace/adminapi/user/entity/Role.java
@@ -1,5 +1,23 @@
 package com.itplace.adminapi.user.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    GUEST, USER, ADMIN
+    GUEST("ROLE_GUEST"),
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String key;
+
+    public static Role fromKey(String key) {
+        for (Role role : Role.values()) {
+            if (role.getKey().equals(key)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant for key " + key);
+    }
 }
