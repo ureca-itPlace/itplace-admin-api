1589fb9563ee2dfa90564fe0f46d9c728684dc14
hyunnk <jhyunnk30@gmail.com>
2025-07-23 09:22:09 +0900

    feat: 혜택 내용 수정, 전체 혜택 수 조회

--- src/main/java/com/itplace/adminapi/benefit/BenefitCode.java
+++ src/main/java/com/itplace/adminapi/benefit/BenefitCode.java
@@ -14,7 +14,10 @@ public enum BenefitCode implements BaseCode {
     BENEFIT_TYPE_NOT_FOUND("BENEFIT_TYPE_NOT_FOUND", HttpStatus.BAD_REQUEST, "존재하지 않는 benefit type입니다."),
     MAIN_CATEGORY_NOT_FOUND("MAIN_CATEGORY_NOT_FOUND", HttpStatus.BAD_REQUEST, "존재하지 않는 main category입니다."),
     USAGE_TYPE_NOT_FOUND("USAGE_TYPE_NOT_FOUND", HttpStatus.BAD_REQUEST, "존재하지 않는 usage type입니다."),
-    BENEFIT_FAVORITE_SUCCESS("BENEFIT_FAVORITE_SUCCESS", HttpStatus.OK,"즐겨찾기 통계가 성공적으로 조회되었습니다.");
+    BENEFIT_FAVORITE_SUCCESS("BENEFIT_FAVORITE_SUCCESS", HttpStatus.OK,"즐겨찾기 통계가 성공적으로 조회되었습니다."),
+    BENEFIT_NOT_UPDATED("BENEFIT_NOT_UPDATED", HttpStatus.BAD_REQUEST, "변경된 값이 없습니다."),
+    BENEFIT_UPDATE_SUCCESS("BENEFIT_UPDATE_SUCCESS", HttpStatus.OK, "혜택 정보가 성공적으로 수정되었습니다."),
+    BENEFIT_COUNT_SUCCESS("BENEFIT_COUNT_SUCCESS", HttpStatus.OK, "전체 혜택 개수가 성공적으로 조회되었습니다.");
 
     private final String code;
     private final HttpStatus status;
--- src/main/java/com/itplace/adminapi/benefit/controller/BenefitController.java
+++ src/main/java/com/itplace/adminapi/benefit/controller/BenefitController.java
@@ -2,6 +2,7 @@ package com.itplace.adminapi.benefit.controller;
 
 import com.itplace.adminapi.benefit.BenefitCode;
 import com.itplace.adminapi.benefit.dto.BenefitDetailResponse;
+import com.itplace.adminapi.benefit.dto.BenefitUpdateRequest;
 import com.itplace.adminapi.benefit.dto.FavoriteRankResponse;
 import com.itplace.adminapi.benefit.service.BenefitService;
 import com.itplace.adminapi.common.ApiResponse;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +36,23 @@ public class BenefitController {
     public ResponseEntity<ApiResponse<BenefitDetailResponse>> getBenefitDetail(@PathVariable Long benefitId) {
         BenefitDetailResponse result = benefitService.getBenefitDetail(benefitId);
         ApiResponse<BenefitDetailResponse> body = ApiResponse.of(BenefitCode.BENEFIT_DETAIL_SUCCESS, result);
+        return ResponseEntity.status(body.getStatus()).body(body);
+    }
+
+    @PutMapping("/{benefitId}")
+    public ResponseEntity<ApiResponse<Void>> updateBenefit(
+            @PathVariable Long benefitId,
+            @RequestBody BenefitUpdateRequest request
+    ) {
+        benefitService.updateBenefitInfo(benefitId, request);
+        ApiResponse<Void> body = ApiResponse.ok(BenefitCode.BENEFIT_UPDATE_SUCCESS);
+        return ResponseEntity.status(body.getStatus()).body(body);
+    }
+
+    @GetMapping("/total")
+    public ResponseEntity<ApiResponse<Long>> getBenefitCount() {
+        Long count = benefitService.getBenefitCount();
+        ApiResponse<Long> body = ApiResponse.of(BenefitCode.BENEFIT_COUNT_SUCCESS, count);
         return ResponseEntity.status(body.getStatus()).body(body);
     }
 }
