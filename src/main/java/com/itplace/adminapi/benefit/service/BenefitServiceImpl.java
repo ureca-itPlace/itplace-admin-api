e23bbb0a559a412279758faf3e03926005b08f92
hyunnk <jhyunnk30@gmail.com>
2025-07-23 09:34:20 +0900

    feat: Transactional 추가

--- src/main/java/com/itplace/adminapi/benefit/service/BenefitServiceImpl.java
+++ src/main/java/com/itplace/adminapi/benefit/service/BenefitServiceImpl.java
@@ -13,8 +13,10 @@ import com.itplace.adminapi.favorite.repository.FavoriteRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class BenefitServiceImpl implements BenefitService {
 
