4a94d8d997065498cf469a7389571ad2896b0037
Eddie Lee <devleehy@gmail.com>
2025-07-24 20:50:11 +0900

    feat: Cors 설정

--- src/main/java/com/itplace/adminapi/common/WebConfig.java
+++ src/main/java/com/itplace/adminapi/common/WebConfig.java
@@ -9,9 +9,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:5174") // 허용할 Origin
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "http://localhost:8080",
+                        "https://admin.itplace.click",
+                        "https://admin-api.itplace.click")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 허용 여부
+                .allowCredentials(true) // 쿠키 허용 여부
+                .maxAge(3600);
     }
 }
